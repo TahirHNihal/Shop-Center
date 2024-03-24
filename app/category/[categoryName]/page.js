20d8b5e4c018a1cdd971a466d5c9d55e8671f62a
TahirHNihal <tahirhnihal29@gmail.com>
2024-03-24 21:10:42 +0600

    Updated

--- app/category/[categoryName]/page.js
+++ app/category/[categoryName]/page.js
@@ -18,7 +18,7 @@ const CategoryPage = ({ params: { categoryName } }) => {
         setCatProducts(newCatProducts);
       }
     }
-  }, [categoryName, catProducts]);
+  }, [categoryName]);
   return (
     <>
       <div className="sticky top-0 right-0 w-full lg:w-10/12 grid grid-cols-2 gap-4 lg:grid-cols-3 my-4 lg:my-10">
