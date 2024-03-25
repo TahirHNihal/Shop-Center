132fb15abf05a32ee1389f695cac2a02a4a7bb6c
TahirHNihal <tahirhnihal29@gmail.com>
2024-03-25 15:20:28 +0600

    Active bar added on category menu

--- components/CategorySideBar.jsx
+++ components/CategorySideBar.jsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const CategorySideBar = () => {
   const router = useRouter();
+  const pathname = usePathname();
+
   const handleCategory = (e) => {
     let value = e.target.value;
     router.push(`/category/${value}`);
@@ -12,28 +14,36 @@ const CategorySideBar = () => {
     <>
       <div className="w-full flex items-center justify-between lg:block lg:w-2/12 my-10 lg:my-0 lg:mt-4">
         <button
-          className="hover:border-b border-black block h-6 box-border mt-4"
+          className={`hover:border-b border-black block h-6 box-border mt-4 ${
+            pathname === "/category/all" ? "active" : ""
+          }`}
           value={"all"}
           onClick={(e) => handleCategory(e)}
         >
           All
         </button>
         <button
-          className="hover:border-b border-black block h-6 box-border mt-5"
+          className={`hover:border-b border-black block h-6 box-border mt-4 ${
+            pathname === "/category/smartphones" ? "active" : ""
+          }`}
           value={"smartphones"}
           onClick={(e) => handleCategory(e)}
         >
           Smartphones
         </button>
         <button
-          className="hover:border-b border-black block h-6 box-border mt-5"
+          className={`hover:border-b border-black block h-6 box-border mt-4 ${
+            pathname === "/category/laptops" ? "active" : ""
+          }`}
           value={"laptops"}
           onClick={(e) => handleCategory(e)}
         >
           Laptops
         </button>
         <button
-          className="hover:border-b border-black block h-6 box-border mt-5"
+          className={`hover:border-b border-black block h-6 box-border mt-4 ${
+            pathname === "/category/fragrances" ? "active" : ""
+          }`}
           value={"fragrances"}
           onClick={(e) => handleCategory(e)}
         >
